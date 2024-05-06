83d97d634ad44365a4803080e17f76c7ba1b68ee
Mo-salakh <mokhmadsalakh.salamov@gmail.com>
2024-05-06 22:31:53 +0300

    50%

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,77 @@
-import '../scss/style.scss'
+import '../scss/style.scss';
+//cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js';
 
-console.log('Works!')
+ps: console.log('Works!')
+
+let elementsParent = document.querySelector('.mySwiper');
+let textShow = document.getElementsByClassName('btn-show');
+let textChanged = textShow.lastElementChild;
+let iconChanged = textShow.firstElementChild;
+let isHidden = false;
+
+
+textShow.addEventListener('click', function () {
+  if (isHidden) {
+    // textChanged.textContent = 'Показать все';
+    elementsParent.style.height = '170px'
+    iconChanged.classList.add('icon-open')
+    iconChanged.classList.remove('icon-hide')
+  } else {
+    // textChanged.textContent = 'Скрыть';
+    elementsParent.style.height = 'auto'
+    iconChanged.classList.add('icon-hide')
+    iconChanged.classList.remove('icon-open')
+  }
+  isHidden = !isHidden
+});
+
+// burger-navigation
+const openButton = document.querySelector('.burger');
+const closeButton = document.querySelector('.burger_close');
+const sidebar = document.querySelector('.burger_navigation');
+let switcher = true;
+
+function openMenuButton() {
+  if (switcher) {
+    Object.assign(sidebar.style, {
+      display: 'block',
+      position: 'absolute',
+      top: '0'
+    })
+  } else {
+    sidebar.style.display = 'none'
+  }
+
+  switcher = !switcher
+};
+
+function closeMenuButton() {
+  sidebar.style.display = 'none'
+  switcher = !switcher
+};
+
+openButton.addEventListener('click', openMenuButton);
+closeButton.addEventListener('click', closeMenuButton);
+
+// Swyper
+const swiper = new Swiper('.swiper', {
+  direction: 'horizontal',
+  loop: true,
+
+  slidesPerView: 'auto',
+  spaceBetween: 16,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true
+  },
+
+  breakpoints: {
+    780: {
+      toggle: false,
+      loop: false,
+      spaceBetween: 'auto',
+      slidesPerView: 'auto',
+      centeredSlides: false
+    }
+  }
+})
